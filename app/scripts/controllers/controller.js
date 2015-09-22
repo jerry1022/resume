3fdc73bdaa14e9c5c401d05c6d9f8d4f6e9526e7
Jerry Huang <jerry_huang@trend.com.tw>
2015-09-22 22:23:53 +0800

    modify basic data

--- app/scripts/controllers/controller.js
+++ app/scripts/controllers/controller.js
@@ -1,29 +1,29 @@
-var myAppModule = angular.module('ResumeControllers', []); 
+var myAppModule = angular.module('ResumeControllers', []);
 
 myAppModule.controller('ResumeController', function($scope, $location) {
-      
+
   var someText = {};
   someText.message = "Jerry's Resuem";
   $scope.someText = someText;
-  
+
   var headerItems = [{itemName: "Profile"}, {itemName: "Education"}, {itemName: "Skill"}, {itemName: "Experience"}, {itemName: "Portfolio"}];
   $scope.headerItems =  headerItems;
-  
+
   // this.msg =  "Jerry's msg";
-   
+
   // var changeTitle =  function () {
   //   var msg = "Jerry's " + $location.path().split("/")[1];
   //     $scope.someText = someText;
   //   };
 
-  // $scope.$watch("$location.path()", this.changeTitle);  
+  // $scope.$watch("$location.path()", this.changeTitle);
 
 });
 
 myAppModule.controller('ExperienceCtrl', function ($scope) {
-  var projects = [{icon : "IBM.png", title: "Advisory IT Specialist", content: "Cloud Infastructure (Sofltlayer and IBM CMS) planning and delivery"}, 
-                  {icon : "promise.png", title: "RAID FW Engineer", content: "RAID Core firmware developement and degug  Promise VTrack – Promise VTrack is a Storage subsystem Responsibility: I was worked in Promise USA to support Firmware development since 2013/6~2013/9, I am in charge of the raid core development. Implement OfflineLD feature"},                      
-                  {icon : "trendmicro.png", title: "Cloud Software Engineer", content: "http://hicloud.hinet.net/s3/products.html hicloud S3"}];
+  var projects = [{icon : "IBM.png", title: "Advisory IT Specialist", content: "Cloud Infastructure (Sofltlayer and IBM CMS) planning and delivery Using Softlayer API implement internal utility and monitoring tools Cloud Computing infra implement Assisted HCMF and  AcBel moved their ERP to the softlayer Large Scale Architecture planning, provision and middleware setting Assisted CHB upgrade middle ware (MQ, WAS and DB2)"},
+                  {icon : "promise.png", title: "RAID FW Engineer", content: "RAID Core firmware developement and degug  Promise VTrack – Promise VTrack is a Storage subsystem Responsibility: I was worked in Promise USA to support Firmware development since 2013/6~2013/9, I am in charge of the raid core development. Implement OfflineLD feature"},
+                  {icon : "trendmicro.png", title: "Cloud Software Engineer", content: "http://hicloud.hinet.net/s3/products.html hicloud S3, Safesync S3 (hicloud  S3) Project – Safesync S3 project is the project which clone the amazon simple storage service, that provide the cloud storage service like aws s3."}];
   $scope.projects = projects;
   });
 
@@ -43,7 +43,7 @@ myAppModule.controller('SkillCtrl', function ($scope, $location) {
       };
 
   drawBars(10);
-  
+
   var getPath =  function($location) {
     var url = $location.path();
     return url;
@@ -52,19 +52,14 @@ myAppModule.controller('SkillCtrl', function ($scope, $location) {
   var url = $location.path().split('/')[1];
   $scope.url =  getPath($location);
   //$scope.url = getPath;
-  
+
 
   });
 
 
 // function ExperienceController($scope) {
-//   var projects = [{icon : "IBM.png", title: "Advisory IT Specialist", content: "Cloud Infastructure (Sofltlayer and IBM CMS) planning and delivery"}, 
-//                   {icon : "promise.png", title: "RAID FW Engineer", content: "RAID Core firmware developement and degug"},                      
+//   var projects = [{icon : "IBM.png", title: "Advisory IT Specialist", content: "Cloud Infastructure (Sofltlayer and IBM CMS) planning and delivery"},
+//                   {icon : "promise.png", title: "RAID FW Engineer", content: "RAID Core firmware developement and degug"},
 //                   {icon : "trendmicro.png", title: "Cloud Software Engineer", content: "http://hicloud.hinet.net/s3/products.html hicloud S3"}];
 //   $scope.projects = projects;
 // }
-
-
-
-
-      
